9589ab96738639f1dc8ec86458696a46e60b9b14
xandremagno10 <xandremagno10@gmail.com>
2025-07-17 21:10:50 +0200

    Faros La Palma Plantilla

---
+++ config.js
@@ -0,0 +1,117 @@
+var config = {
+    style: 'mapbox://styles/pepemagno10/cmd7doq2k003d01r14vaaam3b',
+    // leave commented to use Mapbox Standard Style
+    accessToken: 'pk.eyJ1IjoicGVwZW1hZ25vMTAiLCJhIjoiY21jbzFkcjJ4MDNybzJpc2NlNHNrNTNuZyJ9.7rhCxgZ1s8vJXL8B7FMC7w',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    insetOptions: {
+        markerColor: 'orange'
+    },
+    insetPosition: 'bottom-right',
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Faros La Palma',
+    subtitle: 'Storymaps de faros de la palma',
+    byline: 'By a I.M. Amapper',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'Faro 1',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/Faro_de_Punta_Lava_2014-11.JPG',
+            description: 'The first chapter contains a title, image, and camera view for San Francisco, California. Update the chapter data to make it your own.',
+            location: {
+                center: [-17.925673596632965, 28.59667959686752],
+                zoom: 12.5,
+                pitch: 67,
+                bearing: 57.60
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Faro 2',
+            image: './assets/washington-dc.jpg',
+            description: 'The second chapter flies to Washington, D.C., updates the camera pitch, and slowly rotates. <br>  <br> Washington, D.C., the capital of the United States, is a vibrant city known for its iconic landmarks, including the White House, the U.S. Capitol, and the Washington Monument. It serves as the political heart of the nation and a center for history, culture, and international diplomacy.',
+            location: {
+                center: [-17.843109474937688, 28.455272910355376],
+                zoom: 8.5,
+                pitch: 60,
+                bearing: -43.2,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                //speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Faro 3',
+            image: './assets/geneva.jpg',
+            description: 'Geneva, Switzerland, is a picturesque city nestled along the shores of Lake Geneva, surrounded by the Alps and Jura mountains. Known as a global hub for diplomacy and finance, it is home to numerous international organizations, including the United Nations and the Red Cross.',
+            location: {
+                center: [-17.760448411133275, 28.569916090400262],
+                zoom: 12.52,
+                pitch: 8.01,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Faro 4',
+            image: './assets/buenos-aires.jpg',
+            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            location: {
+                center: [-17.77805263624689, 28.83904923862581],
+                zoom: 4,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
